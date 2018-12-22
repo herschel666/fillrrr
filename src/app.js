b38da640de076debec33e01aa612286adde5d510
Emanuel Kluge <mail@emanuel-kluge.de>
2018-12-22 13:51:16 +0100

    refactor: use Normalize.css from common CDN

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const App = () => (
       <title>Fillrrr</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="robots" content="noindex, nofollow, noarchive" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      />
     </Helmet>
     <Switch>
       <Route exact path="/" component={Home} />
