302f7bdf61a6b0a5bf7ac301677d82c2b5639ee4
Emanuel Kluge <mail@emanuel-kluge.de>
2018-12-23 14:12:12 +0100

    feat: add working 404 page

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,11 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
-import { dark } from './ui/_colors';
+import { dark, primary } from './ui/_colors';
 
 export const GlobalStyles = createGlobalStyle`
 body {
   font: normal 100%/1.2 sans-serif;
   color: ${dark};
   background: white;
+}
+
+a {
+  color: ${primary};
+
+  &:hover,
+  &:focus {
+    text-decoration: none;
+  }
 }
 `;
---
+++ src/pages/not-found/elements.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  margin: 16px;
+`;
+
+export const Title = styled.h1`
+  font-weight: normal;
+`;
