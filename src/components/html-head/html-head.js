38444927be58b7240a6e9f490696fa2bb6c0b0d6
Emanuel Kluge <mail@emanuel-kluge.de>
2018-12-23 22:00:14 +0100

    feat: add favicon & app icons

--- src/components/html-head/html-head.js
+++ src/components/html-head/html-head.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import { dark } from '../../ui/_colors';
+import favicon from '../../icons/favicon.ico';
+import appleTouchIcon from '../../icons/apple-touch-icon.png';
+import favicon32 from '../../icons/favicon-32x32.png';
+import favicon16 from '../../icons/favicon-16x16.png';
 
 export const HtmlHead = () => (
   <Helmet>
@@ -13,5 +17,9 @@ export const HtmlHead = () => (
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
     />
+    <link rel="icon" type="image/x-icon" href={favicon} />
+    <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+    <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+    <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
   </Helmet>
 );
